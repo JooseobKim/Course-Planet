fc443cb0443b138545f45091d6062862ca316f09
JooseobKim <outfox100@naver.com>
2021-08-17 05:53:33 +0900

    테스트 커밋

--- server.js
+++ server.js
@@ -37,12 +37,12 @@ mongoose.connect(
   }
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 4250;
 
