27c2d61cd25e1b6dc6e485ee69f93399c3262be8
JooseobKim <outfox100@naver.com>
2021-08-18 12:12:10 +0900

    패스트캠퍼스 스크래핑 변경사항 적용(4)

--- controllers/coursesCtrl.js
+++ controllers/coursesCtrl.js
@@ -117,6 +117,7 @@ const coursesCtrl = {
 
       const browser = await puppeteer.launch({
         headless: false,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
       });
 
       const page = await browser.newPage();
